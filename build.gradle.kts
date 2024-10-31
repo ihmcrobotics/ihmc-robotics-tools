22d0e84bfeefbe4b7192a3faca89510e1fdde042
Robert Griffin <rgriffin@ihmc.us>
2024-10-30 22:35:57 -0500

    :bookmark: 0.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.1.0"
+   version = "0.1.1"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-robotics-tools"
    openSource = true
    maintainer = "Robert Griffin rgriffin@ihmc.org"
