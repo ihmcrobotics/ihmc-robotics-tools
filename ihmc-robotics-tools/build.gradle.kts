ff77abd6323b86f27184eb2e13cfce4d11306199
Dexton Anderson <danderson@ihmc.org>
2024-12-19 16:42:03 -0600

    :arrow_up: scs2 17-0.28.3 :arrow_up: scs-group 0.25.3 :arrow_up: graphics-description 0.26.0

--- ihmc-robotics-tools/build.gradle.kts
+++ ihmc-robotics-tools/build.gradle.kts
@@ -15,7 +15,7 @@ mainDependencies {
    api("us.ihmc:euclid-frame:0.22.2")
    api("us.ihmc:ihmc-yovariables:0.13.3")
    api("us.ihmc:ihmc-yovariables-filters:0.13.3")
-   api("us.ihmc:ihmc-graphics-description:0.25.2")
+   api("us.ihmc:ihmc-graphics-description:0.26.0")
 }
 
 jointKinematicsDependencies {
