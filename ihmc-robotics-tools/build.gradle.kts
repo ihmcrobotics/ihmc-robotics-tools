ec965d64ae63ae4d58608b3e706bc73a89e59170
rgriffin@ihmc.org <rgriffin@ihmc.us>
2025-01-06 13:25:22 -0600

    :arrow_up: ihmc-commons 0.35.0 :arrow_up: ihmc-graphics-description 0.26.0

--- ihmc-robotics-tools/build.gradle.kts
+++ ihmc-robotics-tools/build.gradle.kts
@@ -11,11 +11,11 @@ ihmc {
 }
 
 mainDependencies {
-   api("us.ihmc:ihmc-commons:0.34.0")
+   api("us.ihmc:ihmc-commons:0.35.0")
    api("us.ihmc:euclid-frame:0.22.2")
    api("us.ihmc:ihmc-yovariables:0.13.3")
    api("us.ihmc:ihmc-yovariables-filters:0.13.3")
-   api("us.ihmc:ihmc-graphics-description:0.25.2")
+   api("us.ihmc:ihmc-graphics-description:0.26.0")
 }
 
 jointKinematicsDependencies {
@@ -30,6 +30,6 @@ visualizersDependencies{
 testDependencies {
    api(ihmc.sourceSetProject("joint-kinematics"))
    api("org.apache.commons:commons-math3:3.6.1")
-   api("us.ihmc:ihmc-commons-testing:0.34.0")
+   api("us.ihmc:ihmc-commons-testing:0.35.0")
    api("com.google.guava:guava:18.0")
 }
