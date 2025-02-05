0941a45ec3c7c3ac302a23d890bfb4963abcab0e
Dexton Anderson <danderson@ihmc.org>
2025-02-05 11:57:32 -0600

    :arrow_up: ihmc-commons 0.35.1

--- ihmc-trajectory-optimization/build.gradle.kts
+++ ihmc-trajectory-optimization/build.gradle.kts
@@ -12,7 +12,7 @@ ihmc {
 
 mainDependencies {
    api("net.sf.trove4j:trove4j:3.0.3")
-   api("us.ihmc:ihmc-commons:0.34.0")
+   api("us.ihmc:ihmc-commons:0.35.1")
    api("us.ihmc:ihmc-matrix-library:0.19.1")
 }
 
