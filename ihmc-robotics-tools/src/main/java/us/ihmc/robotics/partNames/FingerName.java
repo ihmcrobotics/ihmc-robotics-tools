c6b6df2f325c199608a7b1bd1ed9c5a294a568fd
Tomasz Bialek <tbialek@ihmc.org>
2025-08-08 16:21:02 -0500

    Add RING finger to FingerName

--- ihmc-robotics-tools/src/main/java/us/ihmc/robotics/partNames/FingerName.java
+++ ihmc-robotics-tools/src/main/java/us/ihmc/robotics/partNames/FingerName.java
@@ -2,9 +2,9 @@ package us.ihmc.robotics.partNames;
 
 public enum FingerName
 {
-   THUMB, INDEX, MIDDLE, PINKY;
-   
+   THUMB, INDEX, MIDDLE, RING, PINKY;
+
    public static final FingerName[] threeFingerValues = new FingerName[] {THUMB, INDEX, MIDDLE};
-   
+
    public static final FingerName[] fourFingerValues = new FingerName[] {THUMB, INDEX, MIDDLE, PINKY};
 }
