1f763d7c8284e30698ee0e277072bf2bd322541a
Dexton Anderson <danderson@ihmc.org>
2025-08-11 09:42:00 -0500

    :arrow_up: ihmc-native-library-loader 2.0.6

--- ihmc-math/build.gradle.kts
+++ ihmc-math/build.gradle.kts
@@ -15,7 +15,7 @@ mainDependencies {
    api("us.ihmc:ihmc-robotics-tools:source")
    api("org.ejml:ejml-simple:0.39")
    api("gov.nist.math:jama:1.0.3")
-   api("us.ihmc:ihmc-native-library-loader:2.0.4")
+   api("us.ihmc:ihmc-native-library-loader:2.0.6")
 }
 
 linearAlgebraDependencies {
